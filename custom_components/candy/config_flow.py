b489b5581d3e12ce2600c3f05d3be28cac592009
GT <788449+gtgt@users.noreply.github.com>
2024-08-26 23:40:44 +0200

    Disable encryption brute-force

--- custom_components/candy/config_flow.py
+++ custom_components/candy/config_flow.py
@@ -19,6 +19,8 @@ _LOGGER = logging.getLogger(__name__)
 
 STEP_DATA_SCHEMA = vol.Schema({
     vol.Required(CONF_IP_ADDRESS): str,
+    vol.Required(CONF_KEY_USE_ENCRYPTION, default=True): bool,
+    vol.Optional(CONF_PASSWORD): str
 })
 
 
@@ -34,33 +36,5 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         if user_input is None:
             return self.async_show_form(step_id="user", data_schema=STEP_DATA_SCHEMA)
-
-        config_data = {
-            CONF_IP_ADDRESS: user_input[CONF_IP_ADDRESS]
-        }
-
-        errors = {}
-        try:
-            async with async_timeout.timeout(40):
-                encryption_type, key = await detect_encryption(
-                    session=async_get_clientsession(self.hass),
-                    device_ip=user_input[CONF_IP_ADDRESS]
-                )
-        except Exception as err:  # pylint: disable=broad-except
-            _LOGGER.exception(err)
-            errors["base"] = "detect_encryption"
-        else:
-            if encryption_type == Encryption.ENCRYPTION:
-                config_data[CONF_KEY_USE_ENCRYPTION] = True
-                config_data[CONF_PASSWORD] = key
-            elif encryption_type == Encryption.NO_ENCRYPTION:
-                config_data[CONF_KEY_USE_ENCRYPTION] = False
-            elif encryption_type == Encryption.ENCRYPTION_WITHOUT_KEY:
-                config_data[CONF_KEY_USE_ENCRYPTION] = True
-                config_data[CONF_PASSWORD] = ""
-
-            return self.async_create_entry(title=CONF_INTEGRATION_TITLE, data=config_data)
-
-        return self.async_show_form(
-            step_id="user", data_schema=STEP_DATA_SCHEMA, errors=errors
-        )
+            
+        return self.async_create_entry(title=CONF_INTEGRATION_TITLE, data=user_input)
